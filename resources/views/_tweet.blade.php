b38d70596e1b7f4ebb330f9980c57ee27c920bb8
lnrdlgtn <leo.ligutanjr@gmail.com>
2020-08-30 16:51:15 +0800

    Design the timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,11 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">John Wick</h5>
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut erat sit amet urna pretium placerat ut in justo. Mauris porta imperdiet est, laoreet dictum lacus fermentum vel.
+        </p>
+    </div>
+</div>
