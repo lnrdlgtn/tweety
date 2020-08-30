ef7cebf2b8089760e712d5884a350be9e921197f
lnrdlgtn <leo.ligutanjr@gmail.com>
2020-08-30 21:45:43 +0800

    Fix bg on app.blade and friends-list.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
                     <div class="lg:flex-1 lg:mx-10" style="max-width: 700px;">
                         @yield('content')
                     </div>
-                    <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
+                    <div class="lg:w-1/6">
                         @include('_friends-list')
                     </div>
                 </div>
